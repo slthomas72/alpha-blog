2896001ba7beacbf73dc4007b565529664d3a8e9
Rails Udemy Course <sthomas72572@gmail.com>
2018-03-23 17:28:49 -0400

    Added video background and description bigger

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,4 @@
 class Article < ActiveRecord::Base
-  validates :title, presence: true, length: { minimum: 3, maximum: 50}
-  validates :description, presence: true, length: { minimum: 10, maximum: 300}
+  validates :title, presence: true, length: { minimum: 3, maximum: 200}
+  validates :description, presence: true, length: { minimum: 10, maximum: 25000}
 end
